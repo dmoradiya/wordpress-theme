8272726753d5bdd5caa44326381aa750eed304b5
dhmec <dharmoradiya@gmail.com>
2020-10-07 14:27:09 -0600

    add css and js files

--- functions.php
+++ functions.php
@@ -9,3 +9,29 @@ register_nav_menus( // We must register nav locations if we want the admin to be
     'footer_menu' => 'Footer Menu',
   )
 );
+
+/**
+ * Enqueue theme stylesheets and scripts.
+ */
+add_action( 'wp_enqueue_scripts', function ()
+{
+  // Enqueue our JavaScript!
+  wp_enqueue_script(
+    // An ID / name for our script.
+    'techcareers-theme-scripts',
+    // The "public" path to the script file.
+    get_theme_file_uri( '/assets/js/scripts.js' ),
+    // Dependencies (jQuery, Bootstrap, etc.)
+    array(),
+    // Version number of the file (based on time to fight browser cache!)
+    strftime( get_theme_file_uri( '/assets/js/scripts.js' ) ),
+    // Should we output this script in the wp_footer()?
+    FALSE
+  );
+  wp_enqueue_style(
+      'techcareers-theme-main-styles',
+      get_theme_file_uri( '/assets/css/main.css' ),
+      strftime( get_theme_file_uri( '/assets/css/main.css' ) ),
+      'all'
+  );
+} );
