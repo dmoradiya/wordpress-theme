3786d7070081b2907b032041e7e67a4f61476567
dhmec <dharmoradiya@gmail.com>
2020-10-07 11:17:01 -0600

    add footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,16 @@
+    <footer>
+        <h2><?php bloginfo( 'title' ); ?></h2>
+        <p>
+            &copy;
+            <?php echo date( 'Y' ); ?>
+            <a href="<?php echo site_url( ); ?>" >
+                <?php bloginfo( 'title' ); ?>
+            </a>
+            All Rights Reserved.
+        </p>
+    </footer>
+    <?php 
+        wp_footer();        
+    ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<p>Hello World!</p>
+<?php get_header(); ?>
+
+<p>This is <strong>index.php</strong></p>
+
+
+
+
+<?php get_footer(); ?>
