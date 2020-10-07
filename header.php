f7c0bebbd9eb04c0ecaebef0948ae8d204cefe1e
dhmec <dharmoradiya@gmail.com>
2020-10-07 12:06:48 -0600

    add single.php and archive.php

--- header.php
+++ header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo( 'title' ); ?></title>
-    <?php   
-        wp_head();
-    ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo( 'title' ); ?></title>
+  <?php
+    // This will output any scripts, styles, meta,
+    // and more that our theme / plugins need.    
+    wp_head();
+  ?>
 </head>
 <body>
-    <h1><?php bloginfo( 'title' ); ?></h1>
+  <h1><?php bloginfo( 'title' ); ?></h1>
